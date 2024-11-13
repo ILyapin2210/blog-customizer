92aec854e22f25b466b09561762e5f8f767e84c5
Ivan Lyapin <lyapin.ivan2210@gmail.com>
2024-11-13 19:41:17 +0500

    1.0

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,147 @@
+import { SyntheticEvent } from 'react';
+import { useState, useRef } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import {
+	fontFamilyOptions,
+	OptionType,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+
+import { useOutsideClickClose } from './hooks/useOutsideClickClose';
+
+type formPropsType = {
+	onSubmit: (formState: formStateType) => void;
+	onReset: () => void;
+};
+
+// Отдельный стейт для формы, поскольку ключи могут принимать значение null для
+// отображения плэйсхолдеров
+
+export type formStateType = {
+	fontFamilyOption: null | OptionType;
+	fontColor: null | OptionType;
+	backgroundColor: null | OptionType;
+	contentWidth: null | OptionType;
+	fontSizeOption: OptionType;
+};
+
+const defaultFormState: formStateType = {
+	fontFamilyOption: null,
+	fontColor: null,
+	backgroundColor: null,
+	contentWidth: null,
+	fontSizeOption: fontSizeOptions[0],
+};
+
+export const ArticleParamsForm = (props: formPropsType) => {
+	const { onSubmit, onReset } = props;
+
+	const [formState, setFormState] = useState<formStateType>(defaultFormState);
+
+	const handleChange = (name: keyof formStateType, value: OptionType) => {
+		setFormState((prevState: formStateType) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	useOutsideClickClose({ isOpen, rootRef, onChange: setIsOpen });
+
+	const showForm = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		onSubmit(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultFormState);
+		onReset();
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			<ArrowButton isOpen={isOpen} onClick={showForm} />
+			<aside
+				className={clsx(styles.container, isOpen && styles[`container_open`])}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					onReset={handleReset}>
+					<Select
+						title='Шрифт'
+						placeholder='Выберите шрифт'
+						selected={formState.fontFamilyOption}
+						onChange={(option) => {
+							handleChange('fontFamilyOption', option);
+						}}
+						onClose={() => {}}
+						options={fontFamilyOptions}
+					/>
+					<RadioGroup
+						name={'fontSize'}
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) => {
+							handleChange('fontSizeOption', option);
+						}}
+					/>
+					<Select
+						title='Цвет шрифта'
+						placeholder='Выберите цвет шрифта'
+						selected={formState.fontColor}
+						onChange={(option) => {
+							handleChange('fontColor', option);
+						}}
+						onClose={() => {}}
+						options={fontColors}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						placeholder='Выберите цвет фона'
+						selected={formState.backgroundColor}
+						onChange={(option) => {
+							handleChange('backgroundColor', option);
+						}}
+						onClose={() => {}}
+						options={backgroundColors}
+					/>
+					<Select
+						title='Ширина контента'
+						placeholder='Выберите ширину контента'
+						selected={formState.contentWidth}
+						onChange={(option) => {
+							handleChange('contentWidth', option);
+						}}
+						onClose={() => {}}
+						options={contentWidthArr}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, SyntheticEvent } from 'react';
 import clsx from 'clsx';
+
+import { useState } from 'react';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
+import { formStateType } from './components/article-params-form/ArticleParamsForm';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +19,45 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// Стейт формы может включать значения null, поэтому поднятии состояния нужно избавиться от них
+
+	function onFormSubmit(formState: formStateType) {
+		const nonNullableOptions: ArticleStateType = {
+			fontFamilyOption:
+				formState.fontFamilyOption || defaultArticleState.fontFamilyOption,
+			fontColor:
+				formState.fontColor || defaultArticleState.fontColor,
+			backgroundColor:
+				formState.backgroundColor || defaultArticleState.backgroundColor,
+			contentWidth:
+				formState.contentWidth || defaultArticleState.contentWidth,
+			fontSizeOption:
+				formState.fontSizeOption || defaultArticleState.fontSizeOption,
+		};
+
+		setArticleState(nonNullableOptions);
+	}
+
+	function onFormReset() {
+		setArticleState(defaultArticleState);
+	}
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onSubmit={onFormSubmit} onReset={onFormReset} />
 			<Article />
 		</main>
 	);
